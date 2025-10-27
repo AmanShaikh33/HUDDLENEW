0d9b0a0a044d931271eed93cfa5a5e5c00456ec8
Aman Shaikh <amanshaikh7238@gmail.com>
2025-10-27 11:26:56 +0530

    new push

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 // ✅ Apply CORS FIRST — before JSON or cookieParser
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
